c1cf8f7a0a8c9b6ef00d525328efea22c2c6eefa
Abraham Hernandez <abraham@abranhe.com>
2018-09-18 01:21:29 -0400

    Create bubble_sort.rb

---
+++ sorting/bubble_sort.rb
@@ -0,0 +1,25 @@
+# Ruby implementation of bubble sort
+#
+# Author: Carlos Abraham Hernandez
+
+def bubbleSort(array)
+   swapped = true
+   while swapped do
+     swapped = false
+     (array.length - 1).times do |i|
+       if array[i] > array[i + 1]
+         array[i], array[i + 1] = array[i + 1], array[i]
+     swapped = true
+       end
+     end
+   end
+   array
+end
+
+# Test
+arr = [46, 24, 33, 10, 2, 81, 50]
+puts "Unsorted array"
+p arr
+
+puts "Sorted array"
+p bubbleSort(arr)
