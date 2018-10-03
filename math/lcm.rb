bf510c61b33a761e8b6b909f34c4ad64a735538a
vitorcarminatti <vitor.carminatti@hotmail.com>
2018-10-02 21:35:01 -0300

    Added function to calculate de lcm between two numbers

---
+++ math/lcm.rb
@@ -0,0 +1,18 @@
+# LCM (Least Common Multiple) of two numbers is the smallest number which can be divided by both numbers.
+
+value_one = gets.chomp.to_i
+value_two = gets.chomp.to_i
+
+def gcd(first, second)
+  if second != 0
+    gcd(second, first%second)
+  else
+    first
+  end
+end
+
+def lcm(first, second)
+  (first * second)/gcd(first, second)
+end
+
+puts lcm(value_one, value_two)
