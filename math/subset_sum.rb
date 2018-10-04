d2f903a3417750ace887abc7a56b6dcca5d11959
John Maguire <jmaguire@mdsol.com>
2018-10-04 12:35:06 -0400

    added comment describing algorithm

--- math/subset_sum.rb
+++ math/subset_sum.rb
@@ -1,3 +1,5 @@
+# Given a set of non negative numbers and a value, sum,
+# find if there is a subset with sum equal to the sum given
 def subset_sum(set, set_length, sum)
   return true if sum == 0
   return false if set_length == 0 && sum != 0
