475d291146f04df8da86321c241ec3a46d487a7e
Francis <mmagon@nerubia.com>
2018-10-05 15:12:10 +0800

    separate files

--- sorting/quick_sort.rb
+++
@@ -1,8 +0,0 @@
-def quick_sort(array)
-  groups = array.group_by { |f| f <=> array.first }
-  less_than    = groups[-1] || []
-  first        = groups[0]  || []
-  greater_than = groups[1]  || []
-
-  quick_sort(less_than) + first + quick_sort(greater_than)
-end
